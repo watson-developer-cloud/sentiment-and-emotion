77a4181dae34ef0cdb82fc897ed36b5185c361ef
Liang Gou <gouliang@gouliang.com>
2016-03-01 09:51:51 -0800

    fix the bug of converting string to number

--- public/js/emoVizApp.js
+++ public/js/emoVizApp.js
@@ -174,7 +174,7 @@ $(document).ready(function() {
         var maxEmScore = 0;
         emoViz.emotionCategories.forEach(function(cate) {
             if (data.emotionAnalysis[cate])
-                maxEmScore = (data.emotionAnalysis[cate] > maxEmScore) ? data.emotionAnalysis[cate] : maxEmScore;
+                maxEmScore = (Number(data.docEmotions[cate])>maxEmScore)? Number(data.docEmotions[cate]): maxEmScore;
         });
         data.emotionAnalysis.NA = (maxEmScore > 0.5) ? 0 : (1 - maxEmScore);
 
